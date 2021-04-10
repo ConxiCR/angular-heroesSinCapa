ebf3b38bc2fd579df55f561ebf1b73727921a7d7
ConxiCR <conxicr@hotmail.com>
2021-04-10 19:20:08 +0200

    5 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms"; //ngModel lives hear
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { AppComponent } from "./app.component";
+import { HeroesComponent } from "./heroes/heroes.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeroesComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HeroesComponent],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
