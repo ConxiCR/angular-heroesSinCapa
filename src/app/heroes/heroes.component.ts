1b4a52b3245d29c30782ade5f73f2aed55214825
ConxiCR <conxicr@hotmail.com>
2021-04-12 09:53:32 +0200

    3 files modified

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 
 import { Hero } from "../hero";
 import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
 
 @Component({
   selector: "app-heroes",
@@ -9,16 +10,22 @@ import { HeroService } from "../hero.service";
   styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
+  selectedHero?: Hero;
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit() {
-    this.getHeroes;
+    this.getHeroes();
   }
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
   getHeroes(): void {
-    this.heroService.getHeroes()
-    .subscribe(heroes => (this.heroes = heroes));
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 }
