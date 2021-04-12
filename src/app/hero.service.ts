7518116d268cddcf8a425c9de034937af79575f6
ConxiCR <conxicr@hotmail.com>
2021-04-12 12:33:03 +0200

    1 file modified

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
-import { Observable, of } from "rxjs";
+
 import { MessageService } from "./message.service";
 
 @Injectable({
@@ -16,10 +18,10 @@ export class HeroService {
     return heroes;
   }
   getHero(id: number): Observable<Hero> {
-  // For now, assume that a hero with the specified `id` always exists.
-  // Error handling will be added in the next step of the tutorial.
-  const hero = HEROES.find(h => h.id === id) as Hero;
-  this.messageService.add(`HeroService: fetched hero id=${id}`);
-  return of(hero);
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id === id) as Hero;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 }
